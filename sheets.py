0c2c6bb5927dbc101a5a12b4ff388123c1e4a4f0
wac1223 <wawawawa09261@gmail.com>
2026-06-27 07:19:16 +0900

    Create sheets.py

---
+++ sheets.py
@@ -0,0 +1,32 @@
+import os
+import json
+import gspread
+from google.oauth2.service_account import Credentials
+
+scope = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive"
+]
+
+if os.path.exists("service_account.json"):
+
+    creds = Credentials.from_service_account_file(
+        "service_account.json",
+        scopes=scope
+    )
+
+else:
+
+    service_account_info = json.loads(
+        os.environ["GOOGLE_SERVICE_ACCOUNT"]
+    )
+
+    creds = Credentials.from_service_account_info(
+        service_account_info,
+        scopes=scope
+    )
+
+client = gspread.authorize(creds)
+
+# ここは今main.pyにあるものをそのまま持ってくる
+spreadsheet = client.open_by_key("1QndFPPD7_-0iFRQe_37oALHeDlJz_kvfvzrZZ1rSlAU")
